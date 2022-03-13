0b59529954430f82e4174087a290b638db71337c
andrewayadwadie <andrew.aiad2013@gmail.com>
2022-03-13 17:58:59 +0200

    setup localization

--- lib/model/devices_model.dart
+++ lib/model/devices_model.dart
@@ -1,27 +1,27 @@
 class DevicesModel {
   final int id;
   final String name;
-  final String date;
-  final String brand;
-  final String countryName;
-  final int tankSize;
+  // final String date;
+  // final String brand;
+  // final String countryName;
+  // final int tankSize;
   DevicesModel({
-    required this.countryName,
-    required this.tankSize,
+    // required this.countryName,
+    // required this.tankSize,
     required this.id,
     required this.name,
-    required this.date,
-    required this.brand,
+    // required this.date,
+    // required this.brand,
   });
 
   factory DevicesModel.fromJson(Map<String, dynamic> jsonData) {
     return DevicesModel(
       id: jsonData['id'],
       name: jsonData['name'],
-      date: jsonData['date'],
-      brand: jsonData['brand'],
-      countryName: jsonData['countryName'],
-      tankSize: jsonData['tankSize'],
+      // date: jsonData['date'],
+      // brand: jsonData['brand'],
+      // countryName: jsonData['countryName'],
+      // tankSize: jsonData['tankSize'],
     );
   }
 }
